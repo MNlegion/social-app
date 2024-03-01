258c2edc1a91f4136ef1f89e09b1c9653cd317ac
MNlegion <rkreuser30@gmail.com>
2024-03-01 16:46:07 -0500

    server.js update with colors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const colors = require('colors');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -6,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`.bgBlack.magenta);
 });
