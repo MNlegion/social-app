0ace41b12803bf9fdaf1cd8d9805d8b9b27450b8
MNlegion <rkreuser30@gmail.com>
2024-03-02 17:55:32 -0500

    server.js setup with middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const colors = require('colors');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 3000;
 
+// connect to MongoDB
+connectDB();
+
+// Initialize express
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import Routes
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+// Error handling
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`.bgBlack.magenta);
