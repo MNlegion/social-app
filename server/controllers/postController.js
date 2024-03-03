efa7c4471f88ab265073c1d7a27c182badb42cb0
MNlegion <rkreuser30@gmail.com>
2024-03-02 19:05:48 -0500

    post controller and routes update, server working

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,8 +1,27 @@
-const Product = require('../models/postModel');
+const Post = require("../models/postModel");
 
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
+const createPost = async (req, res) => {};
+
+const getSinglePost = async (req, res) => {};
+
+const updatePost = async (req, res) => {};
+
+const deletePost = async (req, res) => {};
 
 // exports
 module.exports = {
-
+    getPosts,
+    createPost,
+    getSinglePost,
+    updatePost,
+    deletePost
 };
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getPosts, createPost, getPost, updatePost, deletePost } = require('../controllers/postController');
+const { getPosts, createPost, getSinglePost, updatePost, deletePost } = require('../controllers/postController');
 
 // routes
 router.route('/').get(getPosts).post(createPost);
-router.route('/:id').get(getPost).put(updatePost).delete(deletePost);
+router.route('/:id').get(getSinglePost).put(updatePost).delete(deletePost);
 
 
 // export the router
