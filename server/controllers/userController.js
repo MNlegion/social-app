032ea92bafb5840e581adc55363188275ccbf6e4
MNlegion <rkreuser30@gmail.com>
2024-03-03 22:40:05 -0500

    userController route crud function setup

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+
+// Register a new user
+const registerUser = async (req, res) => {
+
+};
+
+// Login a user
+const loginUser = async (req, res) => {
+
+};
+
+// Get user profile
+const getUserProfile = async (req, res) => {
+
+};
+
+// Update user profile
+const updateUserProfile = async (req, res) => {
+
+};
+
+// exports
+module.exports = {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+};
