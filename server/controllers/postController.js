7c052297edf29e2034c4aff7ec1196a00d62e5cf
MNlegion <rkreuser30@gmail.com>
2024-03-04 17:11:44 -0500

    updated post controller with update post function

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require("../models/postModel");
+const userModel = require("../models/userModel");
 
 const getPosts = async (req, res) => {
   // try {
@@ -25,9 +26,43 @@ const createPost = async (req, res) => {
   res.status(201).json(post);
 };
 
-const getSinglePost = async (req, res) => {};
+const getSinglePost = async (req, res) => {
+  
+};
 
-const updatePost = async (req, res) => {};
+const updatePost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    res.status(200).json(post);
+  } else {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+
+  // check if the user exists
+  if(!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  // check if the user is the owner of the post
+  if(post.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error('You are not authorized to perform this action');
+  }
+
+  // update the post
+  const updatedPost = await Post.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      body: req.body.body,
+    },
+    { new: true }
+  );
+
+  res.status(200).json(updatedPost);
+};
 
 const deletePost = async (req, res) => {};
 
