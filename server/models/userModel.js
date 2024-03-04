6d3125ac8761564ca17e91b84548afef8f4cabed
MNlegion <rkreuser30@gmail.com>
2024-03-03 20:04:16 -0500

    initial user model setup

---
+++ server/models/userModel.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  profileImage: {
+    type: String,
+    required: false,
+    default: "",
+  },
+  bio: {
+    type: String,
+    required: false,
+  },
+  website: {
+    type: String,
+    required: false,
+  },
+  followers: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  following: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  posts: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  savedPosts: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  notifications: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  messages: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
