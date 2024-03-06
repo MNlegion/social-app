02111e4600820050e580b0a5feaa07e643f4961b
MNlegion <rkreuser30@gmail.com>
2024-03-05 21:43:43 -0500

    userController with register function and passwordHash jwt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,51 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
 // Register a new user
 const registerUser = asyncHandler(async (req, res) => {
-    res.send("Register user");
+  const { username, email, password } = req.body;
+
+  // Check if the user already exists by email
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+      res.status(400);
+      throw new Error("User already exists");
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // If the user doesn't exist, create a new User
+  const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+  });
+
+  if (user) {
+      res.status(201).json({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          token: generateToken(user._id),
+      });
+  } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+  }
 });
+
+// Helper function to generate JWT token
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+  });
+};
+
 
 // Login a user
 const loginUser = asyncHandler(async (req, res) => {
