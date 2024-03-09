e2728c6a685aeafd0e5e48e11a30f204318bedc8
MNlegion <rkreuser30@gmail.com>
2024-03-09 13:04:08 -0500

    format update within userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,74 +1,77 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    minlength: 3,
-    maxlength: 20,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    bio: {
+      type: String,
+      required: false,
+    },
+    website: {
+      type: String,
+      required: false,
+    },
+    followers: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    following: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    posts: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    savedPosts: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    notifications: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    messages: {
+      type: Array,
+      default: [],
+      required: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  profileImage: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  bio: {
-    type: String,
-    required: false,
-  },
-  website: {
-    type: String,
-    required: false,
-  },
-  followers: {
-    type: Array,
-    default: [],
-    required: false,
-  },
-  following: {
-    type: Array,
-    default: [],
-    required: false,
-  },
-  posts: {
-    type: Array,
-    default: [],
-    required: false,
-  },
-  savedPosts: {
-    type: Array,
-    default: [],
-    required: false,
-  },
-  notifications: {
-    type: Array,
-    default: [],
-    required: false,
-  },
-  messages: {
-    type: Array,
-    default: [],
-    required: false,
+  {
+    timestamps: true,
+    versionKey: false,
+    collection: "users",
   }
-}, {
-  timestamps: true,
-  versionKey: false,
-  collection: "users",
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
