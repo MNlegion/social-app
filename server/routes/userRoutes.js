995bc723cad73065b8b72735c1aad31d2fd79089
MNlegion <rkreuser30@gmail.com>
2024-03-08 20:46:22 -0500

    userRoute formatting change

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/userController');
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/userController");
 
 // routes
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 // export the router
 module.exports = router;
