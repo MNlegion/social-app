0682828c4a98e14a9576915c6755c9d71e661541
MNlegion <rkreuser30@gmail.com>
2024-03-10 20:55:57 -0400

    auth protect function for user routes

---
+++ server/middleware/auth.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    // Check if the token exists and starts with Bearer
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Get the token from the request headers
+            token = req.headers.authorization.split(" ")[1];
+
+            // Decode the token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Find the user by id and exclude the password
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error("Not authorized, token failed");
+        }
+    }
+
+    // Check if the token exists
+    if (!token) {
+        res.status(401);
+        throw new Error("Not authorized, no token");
+    }
+});
+
+
+
+module.exports = {
+    protect
+};
