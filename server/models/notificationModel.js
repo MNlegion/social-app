5a189f7b675a8948a9a9a16b12be6664a21d19b2
MNlegion <rkreuser30@gmail.com>
2024-03-14 17:33:12 -0400

    notificationModel initialized

--- server/models/notificationModel.js
+++ server/models/notificationModel.js
@@ -2,17 +2,34 @@ const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
     {
-        user: {
+      recipient: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
-        },
-        notifications: {
-        type: Array,
-        default: [],
-        },
+      },
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      read: {
+        type: Boolean,
+        default: false,
+      },
     },
-    { timestamps: true }
-);
+    {
+      timestamps: true,
+      versionKey: false,
+      collection: "notifications",
+    }
+  );
 
 module.exports = mongoose.model("Notification", notificationSchema);
