d5bb9715a4524c1a0bd11fc4f516510fbbb46fd0
MNlegion <rkreuser30@gmail.com>
2024-03-13 22:07:39 -0400

    server.js updated with notificationRoutes

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use("/api/posts", require("./routes/postRoutes"));
 app.use("/api/comments", require("./routes/commentRoutes"));
 app.use("/api/follow", require("./routes/followRoutes"));
 app.use("/api/likes", require("./routes/likeRoutes"));
+app.use("/api/notifications", require("./routes/notificationRoutes"));
 
 // Error handling
 app.use(errorHandler);
