8b13fa11f47b4a5aa4e7b5c8c9157107517378b1
MNlegion <rkreuser30@gmail.com>
2024-03-13 21:56:55 -0400

    commentRoutes formatting update

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getPosts, createPost, getSinglePost, updatePost, deletePost } = require('../controllers/postController');
+const {
+  getPosts,
+  createPost,
+  getSinglePost,
+  updatePost,
+  deletePost,
+} = require("../controllers/postController");
 
 // routes
-router.route('/').get(getPosts).post(createPost);
-router.route('/:id').get(getSinglePost).put(updatePost).delete(deletePost);
-
+router.route("/").get(getPosts).post(createPost);
+router.route("/:id").get(getSinglePost).put(updatePost).delete(deletePost);
 
 // export the router
 module.exports = router;
