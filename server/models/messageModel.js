0a5b03f54d38c02e6ba8480fc27b4d0446a0e7d1
MNlegion <rkreuser30@gmail.com>
2024-03-14 17:32:03 -0400

    messageModel initialized

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -1,20 +1,31 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
-    {
-        conversationId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Conversation",
-        },
-        sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        },
-        text: {
-        type: String,
-        },
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    { timestamps: true }
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    collection: "messages",
+  }
 );
 
 module.exports = mongoose.model("Message", messageSchema);
