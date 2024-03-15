610b979a704b789515f819578322bff42ca97044
MNlegion <rkreuser30@gmail.com>
2024-03-15 13:02:06 -0400

    working on updateUserProfile function in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -198,28 +198,9 @@ const getUserProfile = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
-  if (user) {
-    user.username = req.body.username || user.username;
-    user.email = req.body.email || user.email;
-
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(req.body.password, salt);
-    }
-
-    const updatedUser = await user.save();
-
-    res.status(200).json({
-      _id: updatedUser._id,
-      username: updatedUser.username,
-      email: updatedUser.email,
-      token: generateToken(updatedUser._id),
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
+  
 });
+
 
 module.exports = { registerUser, loginUser, getUserProfile, updateUserProfile };
 
