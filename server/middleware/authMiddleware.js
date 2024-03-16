8fdced0cc9ccb2b45b89a01f5aaa40b6cf7faf97
MNlegion <rkreuser30@gmail.com>
2024-03-16 18:08:46 -0400

    formatting update within auth middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,42 +3,38 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token;
+  let token;
 
-    // Check if the token exists and starts with Bearer
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
-    ) {
-        try {
-            // Get the token from the request headers
-            token = req.headers.authorization.split(" ")[1];
+  // Check if the token exists and starts with Bearer
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get the token from the request headers
+      token = req.headers.authorization.split(" ")[1];
 
-            // Decode the token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Decode the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            // Find the user by id and exclude the password
-            req.user = await User.findById(decoded.id).select("-password");
+      // Find the user by id and exclude the password
+      req.user = await User.findById(decoded.id).select("-password");
 
-            next();
-        } catch (error) {
-            console.error(error);
-            res.status(401);
-            throw new Error("Not authorized, token failed");
-        }
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
+  }
 
-    // Check if the token exists
-    if (!token) {
-        res.status(401);
-        throw new Error("Not authorized, no token");
-    }
+  // Check if the token exists
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
 });
 
-
-
 module.exports = {
-    protect
+  protect,
 };
-
-
