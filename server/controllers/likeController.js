1befb9f60393c5fecec0dab5314794cc1438edf9
MNlegion <rkreuser30@gmail.com>
2024-03-26 22:04:36 -0400

    post unlike function running

--- server/controllers/likeController.js
+++ server/controllers/likeController.js
@@ -36,7 +36,34 @@ const likePost = asyncHandler(async (req, res) => {
 
 // Unlike a post
 const unlikePost = asyncHandler(async (req, res) => {
-  res.send("Unlike a post");
+  const { postId } = req.params;
+
+  // Check if the post exists
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+
+  // Check if the user has already liked the post
+  const alreadyLiked = await Like.findOne({
+    user: req.user._id,
+    post: postId,
+  });
+
+  if (!alreadyLiked) {
+    res.status(400);
+    throw new Error("You have not liked this post");
+  }
+
+  // Delete the like relationship
+  await Like.findByIdAndDelete(alreadyLiked._id);
+
+  // Update the corresponding Post document to remove the like
+  await Post.findByIdAndUpdate(postId, { $pull: { likes: alreadyLiked._id } });
+
+  res.status(200).json({ message: "Post unliked" });
 });
 
 module.exports = {
