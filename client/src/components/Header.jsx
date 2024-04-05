5a5ce2c9ae15d98de6b14bb776ace69b6b6da6c7
MNlegion <rkreuser30@gmail.com>
2024-04-04 22:49:15 -0400

    tab title update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">Social-App</Link>
+        <Link to="/">Social Experiment</Link>
       </div>
       <ul>
         {user ? (
