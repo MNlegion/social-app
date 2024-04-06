ca1f2e8b46e3e98f6d3c52519413a17aac51c93d
MNlegion <rkreuser30@gmail.com>
2024-04-05 22:56:57 -0400

    dashboard link to PostPage working with PostForm and PostItem

--- client/src/components/Posts/PostPage.jsx
+++ client/src/components/Posts/PostPage.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
-import PostForm from './PostForm'
-import PostItem from './PostItem'
-
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import PostForm from "./PostForm";
+import PostItem from "./PostItem";
+import { getUserPosts, reset } from "../../features/posts/postSlice";
 
 function PostPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { posts } = useSelector((state) => state.posts);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+
+    dispatch(getUserPosts(user._id));
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, dispatch]);
+
   return (
-    <div>
-      Test
-      {/* <PostForm />
-      <PostItem /> */}
-    </div>
-  )
+    <>
+      <section className="heading">
+        <h1>{user.username}'s Dashboard</h1>
+      </section>
+
+      <PostForm />
+
+      <section className="content">
+        <h2>Your Posts</h2>
+        {posts.length === 0 ? (
+          <p>No posts found</p>
+        ) : (
+          <ul className="posts">
+            {posts.map((post) => (
+              <PostItem key={post._id} post={post} />
+            ))}
+          </ul>
+        )}
+      </section>
+    </>
+  );
 }
 
-export default PostPage
+export default PostPage;
