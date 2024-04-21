89550af49072235071411f45a72689ed956c84ef
MNlegion <rkreuser30@gmail.com>
2024-04-21 19:44:08 -0400

    test page 2 add

---
+++ client/src/components/TestPage2/TestPage2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from '../Navigation'
+
+function TestPage2() {
+  return (
+    <>
+        <Navigation />
+        <section className="heading">
+            <h1>Test Page 2</h1>
+        </section>
+    </>
+  )
+}
+
+export default TestPage2
